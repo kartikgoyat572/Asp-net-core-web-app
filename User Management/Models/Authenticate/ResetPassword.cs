4a60b7f691fd9677bc7201fc47409bf03e08d218
kartikgoyat572 <158527756+kartikgoyat572@users.noreply.github.com>
2024-04-06 12:31:30 -0400

    Add files via upload

---
+++ User Management/Models/Authenticate/ResetPassword.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User.Management.AP.Models.Authentication.SignUp
+{
+    public class ResetPassword
+    {
+        [Required]
+
+        public string Password { get; set; } = null!;
+
+        [Compare("Password", ErrorMessage = "The password and confirmation do not match.")]
+
+        public string ConfirmPassword { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+        public string Token { get; set; } = null!;
+
+    }
+}
