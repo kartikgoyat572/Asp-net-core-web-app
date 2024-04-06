178737fe67f1e37f644087375b7c8fec8a921452
kartikgoyat572 <158527756+kartikgoyat572@users.noreply.github.com>
2024-04-06 12:35:41 -0400

    Add files via upload

---
+++ Mangement Service/Services/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.Management.Service.Models;
+
+namespace User.Management.Service.Services
+{
+    public interface IEmailService
+    {
+        void SendEmail(Message message);
+    }
+}
