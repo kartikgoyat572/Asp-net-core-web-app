e4da1237f7b768b3b8e5fb0ceab8574e4a73e2d0
kartikgoyat572 <158527756+kartikgoyat572@users.noreply.github.com>
2024-04-06 12:21:09 -0400

    Add files via upload

---
+++ User Management/Migrations/20240329002203_RolesSeeded.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace User.Management.AP.Migrations
+{
+    public partial class RolesSeeded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "8061909f-daac-4372-9426-2724e00ed23f", "2", "User", "User" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "815a7997-c3eb-4b43-96ad-4a5a7836ef07", "3", "HR", "HR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "dca5b57a-d095-46cb-8db8-b33dc1a4ec27", "1", "Admin", "Admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8061909f-daac-4372-9426-2724e00ed23f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "815a7997-c3eb-4b43-96ad-4a5a7836ef07");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dca5b57a-d095-46cb-8db8-b33dc1a4ec27");
+        }
+    }
+}
